625655d3265a906b3425a7ad18106092c95aaee4
mkopsho <michael.kopsho@gmail.com>
2020-10-28 10:18:56 -0400

    fixing .gitignore

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -46,9 +46,10 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'graphql_jwt.middleware.JSONWebTokenMiddleware',
 ]
 
 ROOT_URLCONF = 'hackernews.urls'
