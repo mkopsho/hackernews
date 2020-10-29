c8abde86a1eccfa7cea1841ec845d344110a703b
mkopsho <michael.kopsho@gmail.com>
2020-10-29 13:49:59 -0400

    Add React hackernews-frontend

---
+++ hackernews-frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>
+        Hackner News
+      </h1>
+      <h2>
+        News about Hackner
+      </h2>
+    </div>
+  )
+}
+
+export default Home
